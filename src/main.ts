acf9bff3970c00ce148093579b2501ab2c56c5ab
gkrtod4477 <dltngus98099@naver.com>
2026-05-28 23:31:07 +0900

    fix(realtime): 앱 기동 안정화
    - Realtime 이벤트 발행 순환 의존성 해소\n- WebSocket 어댑터 등록으로 컨테이너 재시작 문제 해결

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { NecoWsAdapter } from './integrations/websocket/neco-ws.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useWebSocketAdapter(new NecoWsAdapter(app));
   app.setGlobalPrefix('v1');
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
